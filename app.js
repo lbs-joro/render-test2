9da260a42d9893b22f599a5e7bf04e38c0dcbac9
lbs-joro <johan.ronnas@ga.lbs.se>
2025-02-18 17:09:02 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+const express = require("express");
+
+const app = express();
+
+const conn = mysql.createConnection({
+    host: 'sql.freedb.tech',
+    password:'2$sZHGh8aXRKPrp',
+    user: 'freedb_johan-admin2',
+    database:'freedb_testfreedb',
+});
+
+
+app.get("/", (req,res)=>{
+    conn.query("SELECT * FROM numbers", (err, result)=>{
+        res.json(result[0]);
+    })
+});
